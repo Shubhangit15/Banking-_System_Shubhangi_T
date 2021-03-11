2b2db2212e709b30475ba173f27f1d6ca5fc6fde
Shubhangit15 <79996186+Shubhangit15@users.noreply.github.com>
2021-03-11 14:17:33 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+	$servername = "sql310.epizy.com";
+        $username = "epiz_28127888";
+        $password = "FGjzkoPKxrZotWV";
+        $database = "epiz_28127888_hfgds";
+
+// Create connection
+        $conn = mysqli_connect($sql310.epizy.com, $epiz_28127888, $FGjzkoPKxrZotWV, $epiz_28127888_hfgds);
+
+	if(!$conn){
+		die("Unable to connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
